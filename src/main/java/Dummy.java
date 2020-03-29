e0bcf4bfd774dda5153811b9e4016eff93afdf25
UnGu Zartex <yentl.kinoo@student.kuleuven.be>
2020-03-29 14:25:07 +0200

    Updated Libs

--- src/main/java/Dummy.java
+++ src/main/java/Dummy.java
@@ -1,2 +1,21 @@
-public class Dummy {
+import GameWorldAPI.GameWorld.*;
+import GameWorldAPI.GameWorldType.*;
+
+import java.util.List;
+
+public class Dummy implements GameWorldType{
+    @Override
+    public List<Action> getAllActions() {
+        return null;
+    }
+
+    @Override
+    public List<Predicate> getAllPredicates() {
+        return null;
+    }
+
+    @Override
+    public GameWorld createNewGameworld() {
+        return null;
+    }
 }
